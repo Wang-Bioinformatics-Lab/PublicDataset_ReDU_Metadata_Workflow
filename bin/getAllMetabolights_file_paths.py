6d0593b555cd7307c4ad690964f329a9686f86a7
YasinEl <yelabiead@ucsd.edu>
2024-04-25 08:01:13 -0700

    utilize updated MTBLS API

--- bin/getAllMetabolights_file_paths.py
+++ bin/getAllMetabolights_file_paths.py
@@ -37,10 +37,6 @@ def safe_api_request(url, retries=3, expected_codes={200}):
   return None
 
 
-public_metabolights_studies = safe_api_request('https://www.ebi.ac.uk:443/metabolights/ws/studies/technology', retries = 1)
-
-#print(public_metabolights_studies)
-
 def get_files_recursive(study_id, directory, headers, files_list, depth=0, max_depth=4):
     if depth > max_depth or 'nmr' in directory.lower():
         return
@@ -60,7 +56,35 @@ def get_files_recursive(study_id, directory, headers, files_list, depth=0, max_d
             new_directory = f"{directory}/{subdirectory_name}" if directory else subdirectory_name
             get_files_recursive(study_id, new_directory, headers, files_list, depth=depth+1, max_depth=max_depth)
 
+
+
+
+def get_all_files(study_id, headers):
+    try:
+        url = f'https://www.ebi.ac.uk:443/metabolights/ws/studies/{study_id}/data-files?search_pattern=FILES%2F**%2F*.*&file_match=true&folder_match=true'
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4XX, 5XX)
+        return response.json()  # Attempt to return the JSON
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err} - Status code: {response.status_code}")
+    except requests.RequestException as err:
+        print(f"Request failed: {err}")
+    except ValueError as json_err:  # Includes JSONDecodeError
+        print(f"JSON decoding failed: {json_err}")
+        print("Response content:", response.text)  # Print the response text that was not JSON
+    return {'files': []}
+
+
+
+
+
+    return filelist_archive
+
             
+       
+def create_usi(row):
+    return f"mzspec:{row['study_id']}:{row['file_path']}"
+     
             
 if __name__ == "__main__":
             
@@ -70,20 +94,31 @@ if __name__ == "__main__":
     
     
     args = parser.parse_args()
-    
+        
+
+    public_metabolights_studies = safe_api_request('https://www.ebi.ac.uk:443/metabolights/ws/studies/technology', retries = 1)
+
+
     # Initialize variables and headers with your API token
     headers = {'user_token': args.user_token}
-    files_list = []
+    data = []
 
-    # Assuming public_metabolights_studies is a list of study IDs
+    # Loop over each study and gather files
     for study in tqdm(public_metabolights_studies, desc="Processing studies"):
         study_id = study['accession']
-        get_files_recursive(study_id, 'FILES', headers, files_list)
+        files_list = get_all_files(study_id, headers)  
+        
+        # Extract each file and append to the list with the study_id
+        for file in files_list['files']:
+            data.append({'study_id': study_id, 'file_path': file['name']})
 
     # Convert the list to a DataFrame
-    files_df = pd.DataFrame(files_list)
+    files_df = pd.DataFrame(data)
 
+    #add  USI
+    files_df['USI'] = files_df.apply(create_usi, axis=1)
 
+    #export tsv file
     files_df.to_csv(args.output_filename, index = False, sep = "\t")
 
 
