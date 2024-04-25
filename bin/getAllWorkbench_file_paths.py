b5385dd2be69d5f52e4a55374d18ac8827cebbba
YasinEl <yelabiead@ucsd.edu>
2024-04-25 08:28:40 -0700

    add WB file request script

---
+++ bin/getAllWorkbench_file_paths.py
@@ -0,0 +1,102 @@
+import os
+import pandas as pd
+import requests
+import argparse
+import tqdm
+from urllib.parse import urlparse, parse_qs
+
+
+# Define a function to extract the first folder with an extension in order to not get the individual files in e.g. agilent "files"
+def extract_first_folder_with_extension(url):
+    filepath = parse_qs(urlparse(url).query).get('F', [None])[0]
+    parts = filepath.split("/")
+
+    # Check for extensions in each part
+    for i, part in enumerate(parts):
+        if "." in part:
+            # If an extension is found, return the path up to and including that part
+            return "/".join(parts[:i + 1])
+
+    # If no extension is found, return the entire filepath
+    return filepath
+
+def _get_metabolomicsworkbench_filepaths(study_id):
+
+    try:
+        dataset_list_url = "https://www.metabolomicsworkbench.org/data/show_archive_contents_json.php?STUDY_ID={}".format(
+            study_id)
+        mw_file_list = requests.get(dataset_list_url).json()
+        workbench_df = pd.DataFrame(mw_file_list)
+
+        workbench_df['raw_sample_name'] = workbench_df['URL'].apply(extract_first_folder_with_extension)
+
+        # workbench_df["USI_file"] = workbench_df["URL"].apply(
+        #     lambda url: f"mzspec:{study_id}:{parse_qs(urlparse(url).query).get('A', [None])[0]}-{parse_qs(urlparse(url).query).get('F', [None])[0]}"
+        # )
+        workbench_df["USI_file"] = workbench_df["URL"].apply(
+            lambda url: f"mzspec:{study_id}:{parse_qs(urlparse(url).query).get('F', [None])[0]}"
+        )
+
+
+
+
+
+        workbench_df["USI_sample"] = workbench_df.apply(
+            lambda
+                row: f"mzspec:{study_id}:{parse_qs(urlparse(row['URL']).query).get('A', [None])[0]}-{row['raw_sample_name']}",
+            axis=1
+        )
+        
+    except KeyboardInterrupt:
+        raise
+    except:
+        workbench_df = pd.DataFrame()
+
+    return workbench_df
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Give an MWB study ID and get a tsv with file paths present in the study.')
+    parser.add_argument("--study_id", "-mwb_id", type=str, help='An MWB study ID such as "ST002050", "ALL" for every study', required=True)
+    parser.add_argument("--output_path", type=str, help='Output file path to tsv file.')
+
+    args = parser.parse_args()
+
+    if args.study_id == "ALL":
+        # Getting all files
+        url = "https://www.metabolomicsworkbench.org/rest/study/study_id/ST/available"
+        studies_dict = requests.get(url).json()
+
+        study_list = []
+        for key in studies_dict.keys():
+            study_dict = studies_dict[key]
+            study_list.append(study_dict['study_id'])
+
+        study_list = list(set(study_list))
+
+        all_results_list = []
+        for study_id in tqdm.tqdm(study_list):
+            try:
+                temp_result_df = _get_metabolomicsworkbench_filepaths(study_id=study_id)
+                all_results_list.append(temp_result_df)
+            except KeyboardInterrupt:
+                raise
+            except:
+                pass
+
+        result_df = pd.concat(all_results_list, axis=0)
+
+    else:
+        result_df = _get_metabolomicsworkbench_filepaths(study_id=args.study_id)
+
+    result_df['study_id'] = result_df['STUDY_ID']
+    result_df['file_path'] = result_df['FILENAME']
+    result_df['USI'] = result_df['USI_file']
+
+    result_df = result_df[['study_id', 'file_path', 'USI']]
+     
+
+    result_df.to_csv(args.output_path, sep='\t', index=False, header=True)
+
+    print(f"Output written to {args.output_path}")
